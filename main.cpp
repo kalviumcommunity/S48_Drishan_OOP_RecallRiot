c9383f8a3d8896f190ab2eeedbcff02218ac5405
Drey108 <92579304+Drey108@users.noreply.github.com>
2024-08-26 21:52:57 +0530

    added 2 classes and 2 member functions

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Card {
+private:
+    char value;
+    bool faceUp;
+
+public:
+    // Constructor
+    Card(char v) : value(v), faceUp(false) {}
+
+    // Member function to flip the card
+    void flip() {
+        faceUp = !faceUp;
+    }
+
+    // Member function to get the card's value
+    char getValue() const {
+        return value;
+    }
+};
+
+class MemoryGame {
+private:
+    vector<Card> cards;
+
+public:
+    // Member function to add a card to the game
+    void addCard(char value) {
+        cards.push_back(Card(value));
+    }
+
+    // Member function to display the values of the cards
+    void displayCards() const {
+        for (const auto& card : cards) {
+            cout << card.getValue() << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    // Instantiating objects
+    MemoryGame game;
+
+    // Adding cards to the game
+    game.addCard('A');
+    game.addCard('B');
+
+    // Displaying the cards
+    game.displayCards();
+
+    return 0;
+}
